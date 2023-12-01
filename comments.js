978258dff16550db1d3f225ae0403a7603bea8a5
Ricardo Alvarez-Correa <ralvarezcm@gmail.com>
2023-12-01 06:23:59 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,31 @@
+// create a web server
+// 1. create a server
+// 2. start the server
+// 3. handle requests
+// 4. respond to requests
+
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+// 1. create a server
+const server = http.createServer((req, res) => {
+  // 3. handle requests
+  const pathName = req.url;
+  // const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+// 2. start the server
+server.listen(8000)
